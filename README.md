1e1bf3486607cb8c191d05c5ed96c31352819e63
Suhail Gupta <suhailgupta03@gmail.com>
2025-01-13 13:00:12 +0530

    fc

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # prettier-alternatives
+
+- https://dprint.dev/plugins/typescript/config/
+- https://biomejs.dev/reference/configuration/#javascript
+
+Unsupported or Different Options
+- useBraces: BiomeJS does not have a direct equivalent for useBraces.
+- bracePosition: No equivalent in BiomeJS. There is https://biomejs.dev/reference/configuration/#javascriptformatterbracketsameline but that targets JSX < 
+No direct equivalent in BiomeJS:
+    - typeLiteral.separatorKind
+    - singleBodyPosition
+    - operatorPosition
+    - preferHanging
+    - spaceAround
+    - memberExpression.linePerExpression
+    ... and others
+
---
+++ dprint.test.js
@@ -0,0 +1,58 @@
+const obj = {
+  'simpleKey': 'value',
+  'complex key': 'value',
+};
+
+// Function with multiple parameters and return statement
+function exampleFunction(param1, param2,) {
+  const obj = {
+    key1: 'value1',
+    key2: 'value2',
+  };
+
+  if (param1 > param2) {
+    return {
+      result: 'param1 is greater',
+    };
+  } else {
+    return {
+      result: 'param2 is greater or equal',
+    };
+  }
+}
+
+function x(a, b, c, c, d, e, g, h, i, k,) {
+  var zz = a + b + c + d + e + g + h + i + k + '..' + '.....' + '.............' + '....................'
+    + '...................................';
+  return zz;
+}
+
+function y() {
+  var zx = x([
+    1,
+    2,
+    3,
+  ], [
+    '..',
+  ],);
+}
+
+// Array with trailing commas
+const array = [
+  'item1',
+  'item2',
+  'item3',
+];
+
+// Object with nested objects
+const nestedObject = {
+  outerKey: {
+    innerKey: 'innerValue',
+  },
+};
+
+// Arrow function
+const arrowFunc = (a, b,) => a + b;
+
+// Export statement
+export { array, arrowFunc, exampleFunction, nestedObject, };
